04a9303a93a7912bc5bbbe5555ceb890218bca15
webdevbiv <80968651+webdevbiv@users.noreply.github.com>
2023-04-08 20:25:04 +0300

    part-03

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,7 +2,7 @@ import { Route, Routes } from 'react-router-dom';
 
 export const App = () => {
   return <Routes>
-    <Route path='' element={<Home/>}>
+    <Route path='/' element={<Home/>}>
       <Route path='' element={}/>
       <Route path='' element={}/>
       <Route path='' element={}/>
